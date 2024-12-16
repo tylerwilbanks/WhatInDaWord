e53dfa7b2e17a335d09ee14f6c35c1489f689059
tylerwilbanks <tyler.wilbanks45@gmail.com>
2024-12-15 23:05:31 -0600

    add question mark animation

--- composeApp/src/commonMain/kotlin/com/minutesock/dawordgame/feature/game/presentation/ui/WordGameNotStartedScreen.kt
+++ composeApp/src/commonMain/kotlin/com/minutesock/dawordgame/feature/game/presentation/ui/WordGameNotStartedScreen.kt
@@ -1,14 +1,17 @@
 package com.minutesock.dawordgame.feature.game.presentation.ui
 
 import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -25,6 +28,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -40,6 +45,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import whatindaword.composeapp.generated.resources.Res
 import whatindaword.composeapp.generated.resources.calendar_today
 import whatindaword.composeapp.generated.resources.infinity
+import whatindaword.composeapp.generated.resources.question_mark
 import whatindaword.composeapp.generated.resources.what_in_da_word
 
 @Composable
@@ -69,10 +75,11 @@ fun WordGameNotStartedScreen(
     }
 
     Column(
-        modifier = modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.background),
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
         Text(
             text = stringResource(Res.string.what_in_da_word),
             fontSize = 32.sp,
@@ -81,6 +88,7 @@ fun WordGameNotStartedScreen(
             color = textColor
         )
         Spacer(modifier = Modifier.height(50.dp))
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -101,34 +109,95 @@ fun WordGameNotStartedScreen(
                 Spacer(modifier = Modifier.height(30.dp))
             }
 
-            Button(
-                onClick = {
-                    navController.navigate(NavigationDestination.PlayGame(gameMode))
-                }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(
-                    modifier = Modifier.size(20.dp),
-                    painter = painterResource(resource = gameModeIconId),
-                    tint = MaterialTheme.colorScheme.background,
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-                Text(
-                    text = "Play", // todo extract
-                    color = MaterialTheme.colorScheme.background,
-                    fontSize = 20.sp
-                )
-                Spacer(modifier = Modifier.width(10.dp))
-                Icon(
-                    modifier = Modifier.size(20.dp),
-                    painter = painterResource(resource = gameModeIconId),
-                    tint = MaterialTheme.colorScheme.background,
-                    contentDescription = null
-                )
+                AnimatedQuestionMark()
+                Button(
+                    onClick = {
+                        navController.navigate(NavigationDestination.PlayGame(gameMode))
+                    }
+                ) {
+                    Icon(
+                        modifier = Modifier.size(20.dp),
+                        painter = painterResource(resource = gameModeIconId),
+                        tint = MaterialTheme.colorScheme.background,
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "Play", // todo extract
+                        color = MaterialTheme.colorScheme.background,
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Icon(
+                        modifier = Modifier.size(20.dp),
+                        painter = painterResource(resource = gameModeIconId),
+                        tint = MaterialTheme.colorScheme.background,
+                        contentDescription = null
+                    )
+                }
+                AnimatedQuestionMark()
             }
         }
     }
+}
 
+@Composable
+private fun AnimatedQuestionMark() {
+    Box {
+        val scale by rememberInfiniteTransition(label = "scale").animateFloat(
+            initialValue = 0.5f,
+            targetValue = 1.2f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(1000, easing = FastOutSlowInEasing),
+                repeatMode = RepeatMode.Reverse
+            ),
+            label = "pulse"
+        )
+
+        val color by rememberInfiniteTransition(label = "color").animateColor(
+            initialValue = MaterialTheme.colorScheme.background,
+            targetValue = MaterialTheme.colorScheme.primary,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        val shadowColor by rememberInfiniteTransition(label = "color").animateColor(
+            initialValue = MaterialTheme.colorScheme.background,
+            targetValue = MaterialTheme.colorScheme.onBackground,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
+        Icon(
+            painterResource(Res.drawable.question_mark),
+            contentDescription = "Pulsing question mark",
+            tint = shadowColor,
+            modifier = Modifier
+                .size(32.dp)
+                .scale(scale)
+                .graphicsLayer {
+                    alpha = scale.coerceIn(0.0f, 0.3f)
+                    translationY = 5f
+                    translationX = 5f
+                }
+        )
+
+        Icon(
+            painterResource(Res.drawable.question_mark),
+            contentDescription = "Pulsing question mark",
+            tint = color,
+            modifier = Modifier
+                .size(32.dp)
+                .scale(scale)
+        )
+    }
 }
 
 @Preview
