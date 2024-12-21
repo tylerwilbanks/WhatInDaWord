da7777bac34fb10be43f74970261a6b073890049
tylerwilbanks <tyler.wilbanks45@gmail.com>
2024-12-21 11:52:14 -0600

    bump android build version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.minutesock.dawordgame"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
     }
     packaging {
         resources {
